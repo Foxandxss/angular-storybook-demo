e11261d7ac9e8e7012480c74c86600174191ac6b
Jesus Rodriguez <jesusr@ideablade.com>
2020-10-21 16:19:12 +0200

    knobs and ellipsis

--- src/app/components/task.stories.ts
+++ src/app/components/task.stories.ts
@@ -1,7 +1,13 @@
-import { action } from '@storybook/addon-actions'
+import { action } from '@storybook/addon-actions';
+import { withKnobs, object } from '@storybook/addon-knobs';
 import { TaskComponent } from './task.component';
+
+// tslint:disable-next-line: max-line-length
+const longTitle = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`;
+
 export default {
   title: 'Task',
+  decorators: [withKnobs],
   excludeStories: /.*Data$/
 };
 
@@ -20,7 +26,7 @@ export const taskData = {
 export const Default = () => ({
   component: TaskComponent,
   props: {
-    task: taskData,
+    task: object('task', { ...taskData }),
     onPinTask: actionsData.onPinTask,
     onArchiveTask: actionsData.onArchiveTask
   }
@@ -44,6 +50,18 @@ export const Archived = () => ({
     task: {
       ...taskData,
       state: 'TASK_ARCHIVED',
+    },
+    onPinTask: actionsData.onPinTask,
+    onArchiveTask: actionsData.onArchiveTask,
+  },
+});
+
+export const LongTitle = () => ({
+  component: TaskComponent,
+  props: {
+    task: {
+      ...taskData,
+      title: longTitle,
     },
     onPinTask: actionsData.onPinTask,
     onArchiveTask: actionsData.onArchiveTask,
